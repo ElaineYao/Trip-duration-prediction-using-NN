5e12d600aa8cb848e5d14612d4719b6f7911efcd
ElaineYao <32926287+ElaineYao@users.noreply.github.com>
2020-07-19 16:04:43 +0800

    Update train.py

--- train.py
+++ train.py
@@ -8,11 +8,11 @@ def train():
     history = model.fit(x=train_X, y=train_Y, validation_data = (val_X, val_Y),
                     batch_size=64,
                       epochs=10)
-    # Calculate its accuracy on testing data
-    _,acc = model.evaluate(test_X, test_Y)
-    print('The accuracy on the testing data is {}.'.format(acc*100))
+    # Calculate its accuracy on validation data
+    _,acc = model.evaluate(val_X, val_Y)
+    print('The accuracy on the validation data is {}.'.format(acc*100))
     # Save the model
-    model.save('model1_5epoch.h5')
+    model.save('model1_10epoch.h5')
 
 
 if __name__ == '__main__':
